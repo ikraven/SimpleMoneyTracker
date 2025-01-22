a4ca2b2fbebf6c0d211faba82d4d565083e63b9c
Borja Suñen <borja2830@gmail.com>
2025-01-22 16:08:09 +0100

    Initial Commit

---
+++ SimpleMoneyTrackerTests/SimpleMoneyTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMoneyTrackerTests.swift
+//  SimpleMoneyTrackerTests
+//
+//  Created by Borja Suñen on 22/1/25.
+//
+
+import Testing
+@testable import SimpleMoneyTracker
+
+struct SimpleMoneyTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
